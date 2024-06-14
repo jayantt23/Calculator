55bd8e085f3c690869a7cb0e2a7269ebb4d2e447
jayantt23 <jayant.sharma@iiitb.ac.in>
2024-06-14 09:53:14 +0530

    Add logic for operations to work once

--- script.js
+++ script.js
@@ -2,11 +2,27 @@ const numbers = document.querySelectorAll(".numbers");
 const display = document.querySelector(".display");
 const AC = document.querySelector("#AC");
 const decimal = document.querySelector("#decimal");
+const operations = document.querySelectorAll(".operations");
+const equals = document.querySelector("#equals");
 
 let firstNumber = undefined;
 let secondNumber = undefined;
-let operator = "";
-let hasNotDecimal = true;
+let operator = undefined;
+
+let isDisplayReplace = true;
+let isOperatorSelected = false;
+let isFirstNumDefined = false;
+
+function displayAll() {
+  console.table(
+    firstNumber,
+    secondNumber,
+    operator,
+    isDisplayReplace,
+    isOperatorSelected,
+    isFirstNumDefined
+  );
+}
 
 function add(a, b) {
   return a + b;
@@ -21,7 +37,11 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-  return Math.round((a / b) * 100) / 100;
+  return a / b;
+}
+
+function round(a) {
+  return Math.round(a * 100) / 100;
 }
 
 function operate(firstNumber, secondNumber, operator) {
@@ -34,8 +54,10 @@ function operate(firstNumber, secondNumber, operator) {
 numbers.forEach((number) => {
   number.addEventListener("click", () => {
     if (display.textContent.length < 9) {
-      if (display.textContent == "0") {
+      if (isDisplayReplace) {
         display.textContent = number.textContent;
+        if (number.textContent != "0") isDisplayReplace = false;
+        if (isOperatorSelected) operator.classList.remove("selected");
       } else {
         display.textContent += number.textContent;
       }
@@ -44,16 +66,60 @@ numbers.forEach((number) => {
 });
 
 decimal.addEventListener("click", () => {
-  if (display.textContent.length < 9 && hasNotDecimal) {
+  if (display.textContent.length < 9 && !display.textContent.includes(".")) {
     display.textContent += ".";
-    hasNotDecimal = false;
   }
 });
 
 AC.addEventListener("click", () => {
   display.textContent = "0";
-  firstNumber = NaN;
-  secondNumber = NaN;
-  operator = "";
-  hasNotDecimal = true;
+  firstNumber = undefined;
+  secondNumber = undefined;
+  if (isOperatorSelected) operator.classList.remove("selected");
+  operator = undefined;
+  isOperatorSelected = false;
+  isDisplayReplace = true;
+});
+
+operations.forEach((operation) => {
+  operation.addEventListener("click", () => {
+    if (!isOperatorSelected) {
+      if (!isFirstNumDefined) firstNumber = Number(display.textContent);
+      operation.classList.add("selected");
+      operator = operation;
+      isOperatorSelected = true;
+      isDisplayReplace = true;
+      isFirstNumDefined = true;
+    } else {
+      if (operator == operation) {
+        firstNumber = undefined;
+        operator.classList.remove("selected");
+        operator = undefined;
+        isOperatorSelected = false;
+        isDisplayReplace = false;
+        isFirstNumDefined = false;
+      } else {
+        operator.classList.remove("selected");
+        operation.classList.add("selected");
+        operator = operation;
+        isOperatorSelected = true;
+        isDisplayReplace = true;
+      }
+    }
+  });
+});
+
+equals.addEventListener("click", () => {
+  if (isFirstNumDefined && isOperatorSelected) {
+    secondNumber = Number(display.textContent);
+    display.textContent = round(
+      operate(firstNumber, secondNumber, operator.textContent)
+    );
+    isDisplayReplace = true;
+    isFirstNumDefined = false;
+    isOperatorSelected = false;
+    firstNumber = undefined;
+    secondNumber = undefined;
+    operator = undefined;
+  }
 });
